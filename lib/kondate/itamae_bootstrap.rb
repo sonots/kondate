2c9a2828090ac14a9647d578aa4d9a32be0beffb
Naotoshi Seo <sonots@gmail.com>
2017-01-27 13:27:13 +0900

    Add hostname to log prefix by tweaking itamae logger

--- lib/kondate/itamae_bootstrap.rb
+++ lib/kondate/itamae_bootstrap.rb
@@ -11,6 +11,7 @@ module Kondate
     end
 
     def bootstrap
+      tweak_logger
       bootstrap_middleware_recipes
       bootstrap_role_recipes
     end
@@ -23,6 +24,17 @@ module Kondate
 
     def include_recipe(recipe)
       @context.include_recipe(recipe)
+    end
+
+    def tweak_logger
+      hostname = node[:hostname]
+      ::Itamae::Logger::Formatter.class_eval do
+        define_method(:call) do |severity, datetime, progname, msg|
+          log = "%s | %s : %s" % [hostname, "%5s" % severity, msg2str(msg)]
+
+          (colored ? colorize(log, severity) : log) + "\n"
+        end
+      end
     end
 
     def bootstrap_middleware_recipes
